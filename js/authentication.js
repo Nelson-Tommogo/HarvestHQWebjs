354d26fc8bbc2fc074077c31e0da99de4e39bf8a
Nelson-Tommogo <nelsontommogo9@gmail.com>
2024-03-25 13:13:50 +0300

    HarvestHQ

--- js/authentication.js
+++ js/authentication.js
@@ -3,13 +3,14 @@
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
-    apiKey: "AIzaSyC5KmqA_ZYVKpIWpZpa-r58Epnk0f0B0CU",
-    authDomain: "harvesthq-fd804.firebaseapp.com",
-    projectId: "harvesthq-fd804",
-    storageBucket: "harvesthq-fd804.appspot.com",
-    messagingSenderId: "968917107456",
-    appId: "1:968917107456:web:a24a838d028bbb1343e9e5",
-    measurementId: "G-B3T1B5TZ9N"
+    apiKey: "AIzaSyApU7_YcSXNXzixSxD8KBRtivt5oVuyj2c",
+    authDomain: "smartmavuno.firebaseapp.com",
+    databaseURL: "https://smartmavuno-default-rtdb.firebaseio.com",
+    projectId: "smartmavuno",
+    storageBucket: "smartmavuno.appspot.com",
+    messagingSenderId: "298910741240",
+    appId: "1:298910741240:web:dbbe6e3dfcfc1568e9c10d",
+    measurementId: "G-N1C5FBGVEE"
   };
 
 
